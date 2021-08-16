875bc502f1d97c220120bcba4fa7b713b1088b71
Kushal <kushalsubedi2@gmail.com>
2021-08-16 10:07:18 +0545

    date found

--- web-scraper.py
+++ web-scraper.py
@@ -13,23 +13,27 @@ class Scraper():
         print('Started web scraping')
         URL = 'https://exam.ioe.edu.np/'
         page = requests.get(URL)
-    
+
         if page.status_code == 200:
             soup = BeautifulSoup(page.content, 'html.parser')
             table = soup.find_all('tr')
             # print(table)
-        
+
             for notices in table:
                 notice_atag = notices.find('a')
                 notice_name = notices.find('span')
+                notice_date = notices.select('td:nth-child(3)')
+                if(len(notice_date) == 1):
+                    print((notice_date[0].text).strip(','))
+                # print(notice_date)
                 if notice_atag == None:
                     continue
                 link_to_notice = URL+notice_atag['href']
-                link = link_to_notice.replace(' ','%20')
+                link = link_to_notice.replace(' ', '%20')
                 # print(notice_name.text)
                 # print(link)
-                
-                self.total_notices.append((notice_name.text,link))
+
+                self.total_notices.append((notice_name.text, link))
 
         else:
             print('Server Error')
@@ -38,11 +42,10 @@ class Scraper():
         mail_server = 'smtp.gmail.com'
         port = 587
 
-        mail = smtplib.SMTP(mail_server,port)
+        mail = smtplib.SMTP(mail_server, port)
         mail.ehlo()
         mail.starttls()
         mail.ehlo()
-        
 
         sender = 'mraurshal@gmail.com'
         recipient = 'kushalsubedi2@gmail.com'
@@ -52,48 +55,12 @@ class Scraper():
         subject = '!NEW NOTICE ARRIVED FROM IOE!'
         body = self.total_notices[0:3]
         msg = f"Subject:{subject}\n\n{body}"
-        mail.sendmail(sender,recipient,msg)
+        mail.sendmail(sender, recipient, msg)
         mail.quit()
 
-    
 
-while True:
-    data = Scraper()
-    data.scraping()
-    notices  =  data.total_notices
-    x = len(notices)
-    print(x)
-    time.sleep(3)
-    print('for y')
-    new_data = Scraper()
-    # data.total_notices = []
-    new_data.scraping()
-    new_notices  =  new_data.total_notices
-    y = len(new_notices)
-    print(y)
-
-
-    if y == (x):
-        print('no new notices found')
-        # data.send_notice()
-        # print('MAIL sent successfully')
-        
-    else:
-        print('notices found')
-        data.send_notice()
-        print('MAIL sent successfully')
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-    
+# while True:
+data = Scraper()
+data.scraping()
+notices = data.total_notices
+x = len(notices)
